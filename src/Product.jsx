ab78696d1931244223cb748615007b4355d15b1d
FIKAYO66 <mishyfikky14@gmail.com>
2026-02-16 09:59:05 +0000

    Switched to CSS modules and updated Product page

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,33 @@
-import './style.css';
+import styles from "./Product.module.css";
+import { BiSolidMessageRoundedDots } from "react-icons/bi";
+
+
+
 
 function Product() {
   return (
-    <div style={{ marginTop: "120px", padding: "20px" }}>
-      <h1>Product Page</h1>
-      <p>This is the product page content.</p>
+    <div className={styles.background}>
+
+        <div className={styles.signup}>
+                    <div>
+            <BiSolidMessageRoundedDots className={styles.icon}/>
+        </div>
+
+      <h1>Sign up for a new account</h1>
+      <p>Already a member? <a href="https://app.userjot.com/auth/signin">Sign in</a></p>
+      <div className={styles.form}>
+        <h1>Email</h1>
+      <input placeholder="Enter your email"></input>
+        <h1 className={styles.password}>Password</h1>
+      <input placeholder="Enter your password"></input>
+      <p>By signing up, you agree to our <a href="https://userjot.com/terms">Terms</a> & <a href="#">Privacy</a>.</p>
+      <button className={styles.buttono}>Sign Up</button>
+      <div className={styles.divider}>
+        <span>or continue with</span>
+      </div>
+      <button>Google</button>
+      </div>
+      </div>
     </div>
   );
 }
